149e2ebdf8ecd055d7b4327ad26910833ba1caa5
TwistedPurpose <sjludwig7@gmail.com>
2016-02-24 17:58:42 -0800

    Added file pull for titles, made rest calls dynamic
    Added dynamic pulls from a file for titles and added changing parameters
    for anime retrieval

--- DataRepository/AnimeFetcher.py
+++ DataRepository/AnimeFetcher.py
@@ -1,7 +1,22 @@
 import requests
 
-url = 'http://myanimelist.net/api/anime/search.xml?q=full+metal'
+fileName = "AnimeTitles.txt"
 
-r = requests.get(url,auth=('',''))
+animeList = []
 
+with open(fileName) as file:
+    for line in file:
+            animeList.append(line)
+
+restfulAnimeTitle = animeList[3].strip()
+
+requestParameters = { 'q': restfulAnimeTitle }
+
+print restfulAnimeTitle
+
+url = 'http://myanimelist.net/api/anime/search.xml'
+r = requests.get(url,auth=('DatabaseChan','R4pperPhotoElectricTail'), params=requestParameters)
+
+print r.url
+print r
 print r.content
