3fa899c7a93081dc92e8ddf0ae387ec27d2f9e94
TwistedPurpose <sjludwig7@gmail.com>
2016-02-24 20:08:25 -0800

    Restful service calls and anime xml parser
    Will need to move out anime class from AnimeFetcher to common file.
    Otherwise added xml parsing for anime and rest requests for MAL web
    service.
    
    TODO: Pull out credentials, create a parser/query for manga, refactor
    into an object, split out manga and anime parsing and rest calls, and
    feed into database creation.

---
+++ Common/Anime.py
@@ -0,0 +1,6 @@
+class Anime: 
+        def __init__(self, rating, title, episodes, description):
+                self.rating = rating
+                self.title = title
+                self.episodes = episodes
+                self.description = description
--- DataRepository/AnimeFetcher.py
+++ DataRepository/AnimeFetcher.py
@@ -1,22 +1,46 @@
 import requests
+import xml.etree.ElementTree as ET
+
+class Anime: 
+        def __init__(self, title, score, episodes, description):
+                self.title = title
+                self.score = score
+                self.episodes = episodes
+                self.description = description
+
+
+def parseAnimeXML(xml):
+        animeXML = ET.fromstring(xml)
+        animeList = []
+        for entry in animeXML:
+                title = entry.find('title').text
+                score = entry.find('score').text
+                episodes = entry.find('episodes').text
+                description = entry.find('synopsis').text
+                animeObj = Anime(title,score,episodes,description)
+                animeList.append(animeObj)
+        return animeList
 
 fileName = "AnimeTitles.txt"
-
+url = 'http://myanimelist.net/api/anime/search.xml'
+animeTitleList = []
 animeList = []
+i = 0
 
 with open(fileName) as file:
     for line in file:
-            animeList.append(line)
+            animeTitleList.append(line)
 
-restfulAnimeTitle = animeList[3].strip()
+for restfulAnimeTitle in animeTitleList:
+        requestParameters = { 'q': restfulAnimeTitle.strip() }
+        r = requests.get(url,auth=('DatabaseChan','R4pperPhotoElectricTail'), params=requestParameters)
+        if (r.status_code == 200):
+                animeList.extend(parseAnimeXML(r.content))
+                i = i + 1
+                if (i % 10 == 0):
+                        print "Fetched " + str(i) + " records so far."
+        else:
+                print "Could not fetch " + restfulAnimeTitle.strip()
 
-requestParameters = { 'q': restfulAnimeTitle }
+        
 
-print restfulAnimeTitle
-
-url = 'http://myanimelist.net/api/anime/search.xml'
-r = requests.get(url,auth=('DatabaseChan','R4pperPhotoElectricTail'), params=requestParameters)
-
-print r.url
-print r
-print r.content
