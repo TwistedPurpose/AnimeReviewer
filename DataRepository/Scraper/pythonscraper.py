204f22e242f537a2e55df90ee5658bfc8a2750c1
kdyung <kyung8888@gmail.com>
2016-02-25 12:20:28 -0800

    Could not find manga list online. Sample code for python scraper added

---
+++ DataRepository/Scraper/pythonscraper.py
@@ -0,0 +1,11 @@
+#easy_install BeautifulSoup4
+#        easy_install lxml
+        
+import urllib2
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(urllib2.urlopen('http://www.bcsfootball.org').read())
+
+for row in soup('table', {'class': 'mod-data'})[0].tbody('tr'):
+  tds = row('td')
+  print tds[0].string, tds[1].string
