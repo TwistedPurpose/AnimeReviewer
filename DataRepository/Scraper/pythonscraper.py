c745b1af5b66480a95b73f6174e6747f9900cd7b
Kdyung <kyung8888@gmail.com>
2016-02-26 01:48:29 +0000

    scraped spacecowboy's anime and manga lists

--- DataRepository/Scraper/pythonscraper.py
+++ DataRepository/Scraper/pythonscraper.py
@@ -4,22 +4,30 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(urllib2.urlopen('http://myanimelist.net/mangalist/spacecowboy?status=1&tag=').read(), "lxml" )
 
 
 #soup = BeautifulSoup(urllib2.urlopen("http://www.anime-planet.com/manga/all?page=1").read(), "lxml" )
 
 #soup = BeautifulSoup(urllib2.urlopen("https://www.mangaupdates.com/series.html?page=1&perpage=100").read(), "lxml" )
 
-table = soup('div', {'id': 'list_surround'})
+outfile = open("spacecowboyanime.txt",'w')
 
+data = []
+pages = {1,2,6}
 
-
-for row in table:
-    row.find_all('a',{'class':'animetitle'})
-    #print row.find('tr', {'class', 'text pad col1'})
-    #print row.find_all('td', {'class', 'text pad col1'})
-#for row in soup('table'):
-  #tds = row('td')
-  #print tds[0].string, tds[1].string
-
+for i in pages:
+	print i
+	mangaurl = 'http://myanimelist.net/mangalist/spacecowboy?status='+str(i)+'&tag='
+	animeurl = 'http://myanimelist.net/animelist/spacecowboy?status='+str(i)+'&tag='
+	url = animeurl
+	print url
+	soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml" )
+	table = soup('div', {'id': 'list_surround'})
+	for row in table:
+		ref1 = row.find_all('a',{'class':'animetitle'})
+		for x in ref1:
+			text = x.get_text()
+			print text.strip()
+			if (len(text) > 0):
+				outfile.write(text.strip().encode("UTF-8") + '\n')
+outfile.close()
