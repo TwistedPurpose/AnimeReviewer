319e6f5f5385e184e453b9c226f5b68b690a6cad
Kdyung <kyung8888@gmail.com>
2016-02-26 01:02:17 +0000

    sadness

--- DataRepository/Scraper/bakamanga.py
+++ DataRepository/Scraper/bakamanga.py
@@ -4,20 +4,20 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(urllib2.urlopen('http://myanimelist.net/mangalist/spacecowboy?status=1&tag=').read(), "lxml" )
+#soup = BeautifulSoup(urllib2.urlopen('http://myanimelist.net/mangalist/spacecowboy?status=1&tag=').read(), "lxml" )
 
 
 #soup = BeautifulSoup(urllib2.urlopen("http://www.anime-planet.com/manga/all?page=1").read(), "lxml" )
 
-#soup = BeautifulSoup(urllib2.urlopen("https://www.mangaupdates.com/series.html?page=1&perpage=100").read(), "lxml" )
+soup = BeautifulSoup(urllib2.urlopen("https://www.mangaupdates.com/series.html?page=1&perpage=100").read(), "lxml" )
 
-table = soup('div', {'id': 'list_surround'})
+table = soup('table', {'class': 'text series_rows_table'})
 
 
 
 for row in table:
-    row.find_all('a',{'class':'animetitle'})
-    #print row.find('tr', {'class', 'text pad col1'})
+    print row.
+    print row.find('tr', {'class', 'text pad col1'})
     #print row.find_all('td', {'class', 'text pad col1'})
 #for row in soup('table'):
   #tds = row('td')
