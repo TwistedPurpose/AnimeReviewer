fb4045a2ca36f63e8bb213c6e9f7ebcc5c0f9f3a
TwistedPurpose <sjludwig7@gmail.com>
2016-02-25 17:51:15 -0800

    Almost done with database commits
    Created database filler, almost working but will not accept list of
    tuples for some reason when adding anime

--- DataRepository/DatabaseFiller.py
+++ DataRepository/DatabaseFiller.py
@@ -1,9 +1,22 @@
 import sqlite3, DatabaseBuilder, AnimeFetcher
 
 class DBFiller:
+        def __init__(self):
+                self.__conn = None
         
-        def addAnime(anime):
-                
+        def connect(self):
+                self.__conn = sqlite3.connect('inorichan.db')
+                cursor = self.__conn.cursor()
+                return cursor
+
+        def close(self):
+                self.__conn.close()
+        
+        def addAnimeListToDB(self,animeList):
+                cursor = self.connect()
+                cursor.execute('''INSERT INTO anime VALUES
+                        (?,?,?,?,?,?,?,?)''',animeList)
+                self.close()
         
         def fillDBWithAnime(self):
                 print "I hope DB Senpai notices me!"
@@ -13,7 +26,16 @@ class DBFiller:
                 fetcher = AnimeFetcher.Fetcher()
                 animeList = fetcher.fetchAnime()
 
+                dbAnimeList = []
+
+                for anime in animeList:
+                        dbAnimeList.append((anime.malId,anime.title,anime.score, anime.animeType,
+                        anime.startDate,anime.endDate,anime.episodes,''))
+
+                print dbAnimeList[1]
+                self.addAnimeListToDB(dbAnimeList)
                 
+                print "All done!  DB Senpai noticed me!"
 
 
 filler = DBFiller()
