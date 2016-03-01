3e3c2f29830b38bfee3a3933e6a4de7ba5787752
TwistedPurpose <sjludwig7@gmail.com>
2016-02-29 20:06:39 -0800

    Figured it out a bit
    X contains information about the samples (every list inside the list is
    a point of data).  Y contains the answer for X.  Predict gives us a
    score based on a sample data piece.

--- DataRepository/LinearRegressionTrial.py
+++ DataRepository/LinearRegressionTrial.py
@@ -2,6 +2,7 @@ import LOWESS, DatabasePlugs
 from sklearn import linear_model
 from sklearn import tree
 from sklearn import svm
+from sklearn.datasets import load_iris
 
 if __name__=='__main__':
     plugs = DatabasePlugs.DatabasePlugs()
@@ -17,19 +18,26 @@ if __name__=='__main__':
         episodeList.append(anime.episodes)
 
     for anime in animeList:
-    	Y.append([anime.episodes])
-    	X.append([anime.episodes, anime.score])
+    	Y.append([anime.score])
+    	X.append([anime.episodes])
 
+    #iris = load_iris()
 
-    
+    #print len(iris.data)
+    #print len(iris.target)
+    #print iris.target
+
+    #decisionTree = tree.DecisionTreeClassifier()
+    #decisionTree = decisionTree.fit(iris.data,iris.target)
+    #print decisionTree.predict([5.9,  3.,   5.1,  1.8])
+
     decisionTree = tree.DecisionTreeClassifier()
     decisionTree = decisionTree.fit(X,Y)
-    print Y
-    print decisionTree.predict_proba([12,6])
+    print decisionTree.predict([200])
 
-    goodluck = svm.SVC()
+    #goodluck = svm.SVC()
 
-    goodluck.fit(X,Y)
+    #goodluck.fit(X,Y)
     #LOWESS.lowess(ratingList, episodeList)
 	#clf = linear_model.LinearRegression()
 	#clf.fit(ratingList, episodeList)
