5625f750576aa1a85f737e366d76d1676ef9943c
Kdyung <kyung8888@gmail.com>
2016-03-02 01:25:44 +0000

    Created page scraper that gets number of users rating

--- DataRepository/Scraper/pagescraper.py
+++ DataRepository/Scraper/pagescraper.py
@@ -2,6 +2,9 @@ import urllib2
 from bs4 import BeautifulSoup
 
 #Scrapes statistics from MyAnimeList Page
+#ratingCount
+#duration
+#Rating()
 
 #outfile = open("Statistics.txt",'w')
 
@@ -9,14 +12,16 @@ data = []
 ids = {15}
 
 for i in ids:
-	print i
+	#print "id "+ str(i)
 	mangaurl = 'http://myanimelist.net/manga/'+str(i)
 	animeurl = 'http://myanimelist.net/anime/'+str(i)
 	url = animeurl
-	print url
+	#print "URL: "+ url
 	soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml" )
+	#get RatingCount
 	ratingTable = soup('div', {'itemprop': 'aggregateRating'})
 	#print table
+	ratingCount = 0
 	for row in ratingTable:
 		ref1 = row.find_all('span', {'itemprop':'ratingCount'})
 		for x in ref1:
@@ -24,4 +29,15 @@ for i in ids:
 			print ratingCount.strip()
 #			if (len(text) > 0):
 #				outfile.write(text.strip().encode("UTF-8") + '\n')
+	#get duration
+	if (url == animeurl):
+		table = soup('td', {'class': 'borderClass'})
+		for row in table:
+			ref1 = row.find_all('div', {'class':'spaceit'})
+			for x in ref1:
+				text = x.get_text()
+				print text.strip()
+	#			if (len(text) > 0):
+	#				outfile.write(text.strip().encode("UTF-8") + '\n')
+	#get Rating
 #outfile.close()
