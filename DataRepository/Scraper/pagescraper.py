2c34200dc475a817caa8b80f204455935f2da822
TwistedPurpose <sjludwig7@gmail.com>
2016-03-03 20:04:08 -0800

    Revert "Restoring pagescraper"
    This reverts commit 5c8e80cd4b7e39977ea0b83a88d7673fffb42fa4.

--- DataRepository/Scraper/pagescraper.py
+++ DataRepository/Scraper/pagescraper.py
@@ -1,4 +1,3 @@
-@@ -1,11 +1,12 @@
 import urllib2
 import sys
 import re #parse Int out of Text
@@ -7,81 +6,60 @@ from bs4 import BeautifulSoup
 #Scrapes statistics from MyAnimeList Page
 #ratingCount
 #duration
-#Rating()
 #Age Rating
 
 #outfile = open("Statistics.txt",'w')
 
-@@ -13,41 +14,46 @@ data = []
-ids = {15}
+def scrapeForExtraData(ids, mediaType):
+	data = []
 
-for i in ids:
-	#print "id "+ str(i)
-	mangaurl = 'http://myanimelist.net/manga/'+str(i)
-	animeurl = 'http://myanimelist.net/anime/'+str(i)
-	url = animeurl
-	if (len(sys.argv) > 2 ) and sys.argv[2] == "manga" :
-		url = mangaurl
-	else:
-		url = animeurl
+	for i in ids:
+		mangaurl = 'http://myanimelist.net/manga/'+str(i)
+		animeurl = 'http://myanimelist.net/anime/'+str(i)
+		if mediaType == "manga" :
+			url = mangaurl
+		else:
+			url = animeurl
 
-	#print "URL: "+ url
-	soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml" )
+		#print "URL: "+ url
 
-	#get RatingCount
-	ratingTable = soup('div', {'itemprop': 'aggregateRating'})
-	#print table
-	ratingCount = 0
-	for row in ratingTable:
-		ref1 = row.find_all('span', {'itemprop':'ratingCount'})
-		for x in ref1:
-			ratingCount = x.get_text().strip()
-#			if (len(text) > 0):
-#				outfile.write(text.strip().encode("UTF-8") + '\n')
-	#get duration
-	duration = ""
-	rating = ""
-	if (url == animeurl):
-		table = soup('td', {'class': 'borderClass'})
-		for row in table:
-			ref1 = row.find_all('div')
-			durationflag = False
-			for x in ref1:
-				text = x.get_text().strip()
-				if text.startswith("Dur"):
-					array = text.split(" ",1)
-					duration = array[1].strip()
-				if text.startswith("Rating"):
-					array = text.split(" ",1)
-					rating = array[1].strip()
-	#			if (len(text) > 0):
-	#				outfile.write(text.strip().encode("UTF-8") + '\n')
-	tup = (i, ratingCount.encode("utf-8"), duration.encode("utf-8"), rating.encode("utf-8"))
-	data.append(tup)
-	#If we are searching an anime page
-	if url == animeurl:
-		#get duration and rating(age)
-		duration = ""
-		rating = ""
-		if (url == animeurl):
-			table = soup('td', {'class': 'borderClass'})
-			for row in table:
-				ref1 = row.find_all('div')
-				durationflag = False
+		try:
+			soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml" )
+
+			#get RatingCount
+			ratingTable = soup('div', {'itemprop': 'aggregateRating'})
+			ratingCount = 0
+			for row in ratingTable:
+				ref1 = row.find_all('span', {'itemprop':'ratingCount'})
 				for x in ref1:
-					text = x.get_text().strip()
-					if text.startswith("Duration"):
-						array = text.split(" ",1)
-						duration = array[1].strip()
-					if text.startswith("Rating"):
-						array = text.split(" ",1)
-						rating = array[1].strip()
+					ratingCount = x.get_text().strip()
+			#If we are searching an anime page
+			if url == animeurl:
+				#get duration and rating(age)
+				duration = ""
+				rating = ""
+				if (url == animeurl):
+					table = soup('td', {'class': 'borderClass'})
+					for row in table:
+						ref1 = row.find_all('div')
+						durationflag = False
+						for x in ref1:
+							text = x.get_text().strip()
+							if text.startswith("Duration"):
+								array = text.split(" ",1)
+								duration = array[1].strip()
+							if text.startswith("Rating"):
+								array = text.split(" ",1)
+								rating = array[1].strip()
 
-		tup = (i, ratingCount.encode("utf-8"), duration.encode("utf-8"), rating.encode("utf-8"))
-		data.append(tup)
-	#If we are searching a manga page, do not return duration and rating
-	else:
-		tup = (i, ratingCount.encode("utf-8"))
-		data.append(tup)
-	print tup
-#outfile.close()
+				tup = (i, int(ratingCount.encode("utf-8").replace(',', '')), int(re.sub("[^0-9]", "", duration.encode("utf-8"))), 
+					rating.encode("utf-8"))
+				data.append(tup)
+			#If we are searching a manga page, do not return duration and rating
+			else:
+				tup = (i, ratingCount.encode("utf-8"))
+				data.append(tup)
+		except:
+			print "Error on ID " + i
+
+	return data
