8438017e8ce58c987ae13db6cbe6ee3be4452675
Kdyung <kyung8888@gmail.com>
2016-03-04 02:00:18 +0000

    Scraper obtains genre string

--- DataRepository/Scraper/pagescraper.py
+++ DataRepository/Scraper/pagescraper.py
@@ -25,39 +25,41 @@ for i in ids:
 	#print "URL: "+ url
 	soup = BeautifulSoup(urllib2.urlopen(url).read(), "lxml" )
 
+	ratingCount = 0
 	#get RatingCount
 	ratingTable = soup('div', {'itemprop': 'aggregateRating'})
-	ratingCount = 0
+	
 	for row in ratingTable:
 		ref1 = row.find_all('span', {'itemprop':'ratingCount'})
 		for x in ref1:
 			ratingCount = x.get_text().strip()
-
+	
 	#If we are searching an anime page
-	if url == animeurl:
-		#get duration and rating(age)
-		duration = ""
-		rating = ""
-		if (url == animeurl):
-			table = soup('td', {'class': 'borderClass'})
-			for row in table:
-				ref1 = row.find_all('div')
-				durationflag = False
-				for x in ref1:
-					text = x.get_text().strip()
-					if text.startswith("Duration"):
-						array = text.split(" ",1)
-						duration = array[1].strip()
-					if text.startswith("Rating"):
-						array = text.split(" ",1)
-						rating = array[1].strip()
+	#get duration, rating(age), and genre
+	duration = ""
+	rating = ""
+	genre = ""	
+	if (url == animeurl):
+		table = soup('td', {'class': 'borderClass'})
+		for row in table:
+			ref1 = row.find_all('div')
+			durationflag = False
+			for x in ref1:
+				text = x.get_text().strip()
+				if text.startswith("Duration"):
+					array = text.split(" ",1)
+					duration = array[1].strip()
+				if text.startswith("Rating"):
+					array = text.split(" ",1)
+					rating = array[1].strip()
+				if text.startswith("Genre"):
+					array = text.split("\n",1)
+					genre = array[1].strip()
 
-		tup = (i, ratingCount.encode("utf-8"), duration.encode("utf-8"), rating.encode("utf-8"))
-		data.append(tup)
+
+	tup = (i, ratingCount.encode("utf-8"), duration.encode("utf-8"), rating.encode("utf-8"), genre.encode("utf-8"))
+	
 	#If we are searching a manga page, do not return duration and rating
-	else:
-		tup = (i, ratingCount.encode("utf-8"))
-		data.append(tup)
-
-
+	#tup = (i, ratingCount.encode("utf-8"))
+	data.append(tup)
 	print tup
