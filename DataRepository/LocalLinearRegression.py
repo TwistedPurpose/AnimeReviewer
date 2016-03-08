19a1cc14635c86eca7f3afb13b159565ddbf536d
TwistedPurpose <sjludwig7@gmail.com>
2016-03-07 19:05:21 -0800

    I have no clue if that test works
    No clue if the test works

--- DataRepository/LocalLinearRegression.py
+++ DataRepository/LocalLinearRegression.py
@@ -85,24 +85,27 @@ if __name__=='__main__':
             sampleXList.append(x)
 
     sampleXList, sampleYList = zip(*sorted(zip(sampleXList, sampleYList)))
+    testXList, testYList = zip(*sorted(zip(testXList, testYList)))
 
     sampleX = np.array(sampleXList)
     sampleY = np.array(sampleYList)
 
+    testX = np.array(testXList)
+    testY = np.array(testYList)
+
     from matplotlib import pyplot as pl
 
     yest = LOWESS.lowess(sampleX, sampleY, iter=10)
+
+    test = np.interp(testX, sampleX, yest)
 
     import matplotlib.pylab as pl
     pl.clf()
     pl.title("")
     pl.scatter(sampleX, sampleY, label=graphLabel)
     pl.plot(sampleX, yest, label='rating prediction', color='red')
+    pl.plot(testX, test, label='test', color='green')
     pl.xlabel(xLabel)
     pl.ylabel(yLabel)
     pl.legend()
     pl.show()
-
-    #Interpolate Values
-    #new_x_values = [0,1,2]
-    #np.interp(new_x_values, lowess[:,0], lowess[:,1])
