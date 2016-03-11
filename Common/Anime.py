da1243fd81ce149526c7adb42ca69cfefb33b694
Kdyung <kyung8888@gmail.com>
2016-03-11 03:03:41 +0000

    Rearranged directory. Deleted empty folders

--- Common/Anime.py
+++
@@ -1,6 +0,0 @@
-class Anime: 
-        def __init__(self, rating, title, episodes, description):
-                self.rating = rating
-                self.title = title
-                self.episodes = episodes
-                self.description = description
